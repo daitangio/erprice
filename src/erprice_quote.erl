7bff07ce822959da0ccc046ffea225fabd06e943
JJ on RaspPi <jj@gioorgi.com>
2016-07-25 21:48:06 +0200

    erprice quote api sketching

--- src/erprice_quote.erl
+++ src/erprice_quote.erl
@@ -8,8 +8,9 @@
 %% For eunit:
 -export([extract24_quote/1,get24price/1]).
 
-%% Provide Adapter for getting quotes
-%% Master Behavior server
+%% @doc Provide Adapter for getting quotes
+%%??? RESEE Responsability: simple market scheduler
+%% 
 
 %% @doc how much parallel connections for getting quote?
 -define(MAX_CONNECTIONS,2).
@@ -17,22 +18,50 @@
 %% @doc delay between two consecutive requests on the same quote resource
 -define(COLD_DOWN_MS,60000).
 
+
+%%% BASIC API
+%% @doc Generate a watchdoc process which will look for the quote at regular intervals.
+%% Example usage:
+%% erprice_quote:watch("ENEL","MI",lessthen,40).
+%% Calling process will receive a 
+%% {notify,Company,Quote}
+%% when threshold is reached
+watch(Company, Market,lessthen,Quote)->
+    {ok,Pid} = gen_server:start_link(?MODULE,[{Company,Market}],[]),
+    gen_server:cast(Pid, {self(),lessthen,Quote}),
+    Pid.
+
+
+%%% GEN SERVER Implementation
+
 start_link()->
     R=gen_server:start_link({local, ?MODULE }, ?MODULE, [], []),
     %%sys:trace(?MODULE,true),    
     R.
 
-init([]) ->
+init([{Company,Market}]) ->
+    error_logger:info_msg("Configured  ~p.~p ",[Company, Market]),
     %% See http://erlang.org/doc/reference_manual/expressions.html#id82225
-    State= #{ max_connections => ?MAX_CONNECTIONS, coldown => ?COLD_DOWN_MS },
+    State= { Company, Market},
     {ok,State}.
 
 handle_call(quote,_From, State) ->
     {reply,use_cast, State}.
 
-handle_cast({quote, _Company,_Market},State)->
-    NewState = State,
-    {noreply,NewState, 10000}.
+handle_cast({CallerPid, lessthen, Quote},State)->
+    error_logger:info_msg("Monitoring lessthen"),
+    { Company, Market } = State,
+    CurrentQuote = case Market of
+        "MI" ->   get24price(string:concat(string:concat(Company,"."),Market)) ;
+        _Other -> getYahooPrice(Company) 
+    end,
+    if CurrentQuote < Quote  -> 
+            CallerPid ! { notify, Company, Quote},
+            error_logger:info_msg("Trigger!");
+       true -> error_logger:info_msg("Bad luck")
+    end,
+    %% Reschedule to myself after a timeout... only if bad luck happens!
+    {noreply,State, 10000}.
 
 %% handle_info({'EXIT', _Pid, _Reason}, State) ->
 %%     %% ..code to handle exits here..
